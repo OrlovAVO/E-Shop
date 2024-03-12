51113f1c067beb289e9775201872cf23c74f2c78
Artyom Orlov <sossanaka000@gmail.com>
2024-03-13 02:47:20 +0500

    update project

--- project/users/views.py
+++ project/users/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
+
+from users.forms import ProfileForm
+
+
 def login(request):
-    form = UserLoginForm()
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                messages.success(request, f'{username}, добро пожаловать')
+
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+
+                return HttpResponseRedirect(reverse('app:index'))
+    else:
+        form = UserLoginForm()
+
     context = {
         'title': 'E-Shop - Авторизация',
         'form': form
@@ -12,20 +36,42 @@ def login(request):
 
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request,user)
+            messages.success(request, f'{user.username}, вы успешно зарегистрированы')
+            return HttpResponseRedirect(reverse('app:index'))
+    else:
+        form = UserRegistrationForm()
     context = {
         'title': 'E-Shop - Регистрация'
 
     }
     return render(request, 'users/registration.html', context)
 
-
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Профиль обновлён')
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+
     context = {
-        'title': 'E-Shop - Кабинет'
+        'title': 'E-Shop - Кабинет',
+        'form': form
 
     }
     return render(request, 'users/profile.html', context)
 
-
+@login_required
 def logout(request):
-    ...
+    messages.success(request, f'{request.user.username}, вы вышли из учётной записи')
+    auth.logout(request)
+    return redirect(reverse('app:index'))
