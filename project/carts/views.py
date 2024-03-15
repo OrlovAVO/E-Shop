c646aaf70e372652be19ab54d5ab9f8dda3c1c23
Artyom Orlov <sossanaka000@gmail.com>
2024-03-15 21:44:45 +0500

    update project

--- project/carts/views.py
+++ project/carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from carts.models import Cart
 from goods.models import Products
@@ -29,4 +29,23 @@ def cart_remove(request, cart_id):
 
 
 def cart_change(request, product_slug):
-    ...
+    if request.method == 'POST':
+        action = request.POST.get("action")  # Получение значения action из запроса
+
+        cart_item = get_object_or_404(Cart, product__slug=product_slug)
+
+        if action == 'add':
+            cart_item.quantity += 1
+            cart_item.save()
+
+        elif action == 'remove':  # Используйте elif вместо if, чтобы проверить разные действия
+            if cart_item.quantity > 1:
+                cart_item.quantity -= 1
+                cart_item.save()
+            else:
+                cart_item.delete()
+
+        return redirect(request.META['HTTP_REFERER'])
+
+    else:
+        return redirect(request.META['HTTP_REFERER'])
