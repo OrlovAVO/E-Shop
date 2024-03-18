5ce1215abbe3f43ff0d3ffb1221e21ef890314f5
Artyom Orlov <sossanaka000@gmail.com>
2024-03-18 12:05:34 +0500

    add css

--- project/app/urls.py
+++ project/app/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 app_name = 'app'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('catalog/vse-tovary/', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('delivery/', views.delivery, name='delivery'),
     path('contacts/', views.contacts, name='contacts')
